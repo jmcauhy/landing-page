1e761b2db616416b4c876b19a109d22fb1f1e61d
jmcauhy <jmcauhy@gmail.com>
2023-08-25 10:18:01 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { createGlobalStyle } from 'styled-components'
+
 function App() {
    return (
       <>
-         <h1>Hello World</h1>
+         <GlobalStyle />
       </>
    )
 }
+
+const GlobalStyle = createGlobalStyle`
+   :root {
+      --dark-blue: #0c4577;
+      --blue: #105ea2;
+   }
+
+   * {
+      margin: 0;
+      padding: 0;
+      outline: none;
+      box-sizing: border-box;
+   }
+
+   body {
+      font-family: 'Montserrat', sans-serif;
+      -webkit-font-smoothing: antialiased;
+   }
+
+   button {
+      font-family: 'Open Sans', sans-serif;
+   }
+`
 
 export default App
