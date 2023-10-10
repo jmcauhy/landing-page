b3566dfd4f69b8b385f248f26f9f2f70851bebdd
jmcauhy <jmcauhy@gmail.com>
2023-10-09 21:19:38 -0300

    Add faqs mock

---
+++ src/services/mocks/faqs.ts
@@ -0,0 +1,29 @@
+import { IFaq } from './faqs.d'
+
+export const faqs: IFaq[] = [
+   {
+      id: 1,
+      question: 'Lorem ipsum dolor sit amet?',
+      answer: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ratione nam quasi fugiat, facilis nobis perferendis ad quaerat tempora modi!',
+   },
+   {
+      id: 2,
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit?',
+      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio perspiciatis consequuntur asperiores enim iure nemo voluptate debitis, pariatur atque! Facere iure molestias commodi voluptates nisi.',
+   },
+   {
+      id: 3,
+      question: 'Lorem dolor sit amet consectetur adipisicing elit?',
+      answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, dolorem ut! Voluptates.',
+   },
+   {
+      id: 4,
+      question: 'Lorem ipsum dolor sit amet consectetur?',
+      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut maxime velit ipsam asperiores esse? Sint, eius perferendis.',
+   },
+   {
+      id: 5,
+      question: 'Lorem ipsum dolor, sit amet consectetur adipisicing?',
+      answer: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem at quidem dolor totam dolorum accusamus, rem, saepe fugit, culpa dolores a consequatur magni delectus eius ipsam quos reiciendis molestiae exercitationem.',
+   },
+]
